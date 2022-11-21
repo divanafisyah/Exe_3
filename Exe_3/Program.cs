0c7e4df01f0b0dc5f7f46a955f9f50430f987a9c
Diva Nafisyah <diva.nafisyah.ft21@mail.umy.ac.id>
2022-11-21 16:36:56 +0700

    add method listEmpty

--- Exe_3/Program.cs
+++ Exe_3/Program.cs
@@ -37,5 +37,13 @@ namespace Exe_3
             else
                 return (false); /*returns false if the node is not found*/
         }
+
+        public bool listEmpty()
+        {
+            if (LAST == null)
+                return true;
+            else
+                return false;
+        }
     }
 }
